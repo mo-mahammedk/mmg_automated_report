0c22ff71abc0721d96e25f1461fbe5268e20e7ef
mo-mahammedk <mahammed.kassam@metoffice.gov.uk>
2024-03-13 16:46:44 +0000

    MKA: Working automated weekly report

--- weekly_reports/friday.py
+++ weekly_reports/friday.py
@@ -2,19 +2,16 @@
 import datetime
 
 friday = datetime.date.today()
-
-
-  
 while friday.strftime('%a') != 'Fri':
-    friday += datetime.timedelta(1)
-    # print (friday)
-    def foo(myDate):
-        date_suffix = ["th", "st", "nd", "rd"]
+    friday += datetime.timedelta()
+# print (friday)
+def foo(myDate):
+    date_suffix = ["th", "st", "nd", "rd"]
 
-        if myDate % 10 in [1, 2, 3] and myDate not in [11, 12, 13]:
-            return date_suffix[myDate % 10]
-        else:
-            return date_suffix[0]
-         
-    worded = friday.strftime(f'%d{foo('%d')} %B %Y')
-    # print(worded)
+    if myDate % 10 in [1, 2, 3] and myDate not in [11, 12, 13]:
+        return date_suffix[myDate % 10]
+    else:
+        return date_suffix[0]
+        
+worded = friday.strftime(f'%d{foo('%d')} %B %Y')
+# print(worded)
